a5ed340eee60b5eef44dea5d184c164156502e38
salmangunyih <salmamwangi25@gmail.com>
2025-09-25 22:07:17 +0300

    Add files via upload

---
+++ loan.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/*
+NAME:cecilia muthoni iregi 
+REG:CT100/G/26194/25
+DESCRIPTION: loan qualification
+*/
+
+int main()
+{
+float age;
+float income;
+// prompt user for inputs 
+ printf("enter your age");
+ scanf("%d",age);
+ 
+ printf("enter your income");
+ scanf("%d",income);
+ 
+ //check loan qualifications 
+ if (age>=21&&income>=21000)
+ {
+ printf("congratulations you qualify for a loan");
+ }
+ else
+ {
+ printf("unfortunately,we are unable to offer you loanat this time");
+ }
+ 
+ return 0;
+ }
---
+++ surface area and volume.c
@@ -0,0 +1,29 @@
+/*
+NAME:cecilia muthoni iregi 
+REG:CT100/G/26194/25
+DESCRIPTION :surface area and volume of a cylinder 
+#define PI 3.14159 ;// define Pi value';'
+*/
+int  main  () {
+float radius;
+float height;
+float PI=3.13159;
+// prompt user for inputs 
+printf("enter radius of the cylinder:");
+scanf("%f",&radius);
+
+printf("enter height of the cylinder");
+scanf("%f",&height);
+
+// calculate  volume and surface area 
+  float volume=PI*radius*radius*height ; 
+ //πr2h';'
+ float surfacearea =2*PI*radius*radius+2*PI*radius*height;
+ //2πr2+2πrh ';'
+ // display results 
+ printf("\n ___ cylinder results___\n");
+ printf("volume=    %.2f\n",volume);
+ printf("surface area=   %.2f \n",surfacearea);
+ return 0;
+ }
+ 
